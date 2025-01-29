a61cd5920124790cb5c3304b92d613a72affb9c0
aqilirwana <77451972+aqilirwana@users.noreply.github.com>
2025-01-29 08:11:27 +0800

    Revert the previous cors change config in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors';
 dotenv.config();
 import connectDB from './config/db.js';
 import cookieParser from 'cookie-parser';
@@ -13,8 +12,6 @@ const port = process.env.PORT || 4000;
 connectDB();
 
 const app = express();
-
-app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
