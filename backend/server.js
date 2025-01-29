0103b79640816d4aa0023c9c4ab22fa3a5ec86e8
aqilirwana <77451972+aqilirwana@users.noreply.github.com>
2025-01-29 08:08:49 +0800

    Installing cors & configure cors in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 dotenv.config();
 import connectDB from './config/db.js';
 import cookieParser from 'cookie-parser';
@@ -12,6 +13,8 @@ const port = process.env.PORT || 4000;
 connectDB();
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
